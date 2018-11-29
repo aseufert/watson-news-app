f2039da2110e52d8813b991c8300824a077a73a3
Alex Seufert <amseufert@gmail.com>
2018-11-28 20:01:17 -0500

    Added First Watson Functionality

--- main.py
+++ main.py
@@ -9,14 +9,31 @@ with open('credentials.json') as f:
     watson_key = data['watson_key']
 
 
+def watson(text):
+    url = 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone'
+    headers = {
+        'content-type': 'application/json'
+    }
+    params = (
+        ('version', '2017-09-21'),
+        ('sentences', False)
+    )
+    data = {
+        'text': text
+    }
+    resp = requests.post(url, headers=headers, params=params, json=data, auth=('apikey', watson_key)).json()
+
+    pprint(resp)
+
+
 def main():
     url = 'https://newsapi.org/v2/top-headlines?country=us&sortBy=popularity&apiKey={}'.format(news_key)
     resp = requests.get(url, timeout=5).json()
-    pprint(resp)
+    top_headline = resp['articles'][0]['title']
 
-    for a in resp['articles']:
-        if a['content']:
-            print(a['content'])
+    print(top_headline)
+
+    watson(top_headline)
 
 
 if __name__ == '__main__':
